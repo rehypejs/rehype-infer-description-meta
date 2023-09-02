e377e6e6bc1ba18aaf1a26198c0d7c137bc9d092
Titus Wormer <tituswormer@gmail.com>
2023-09-02 10:04:22 +0200

    Refactor `tsconfig.json`

--- complex-types.d.ts
+++ complex-types.d.ts
@@ -1,24 +1,33 @@
 import type {Root, Element} from 'hast'
 
-// Need a random export to turn this into a module?
-export type Whatever = unknown
+interface InferDescriptionMeta {
+  /**
+   * Description of the document (optional, example: `'The city has changed
+   * drastically over the past 40 years, yet the M.T.A. map designed in 1979
+   * has largely endured.'`).
+   *
+   * Inferred by `rehype-infer-description-meta` from HTML.
+   * Used by `rehype-meta`.
+   */
+  description?: string | null | undefined
+  /**
+   * Description of this document, in hast tree form (optional).
+   *
+   * Inferred by `rehype-infer-description-meta` from HTML.
+   * Used by `rehype-meta`.
+   */
+  descriptionHast?: Root | Element | null | undefined
+}
 
+// Add custom data supported when `rehype-infer-description-meta` is added.
 declare module 'vfile' {
-  interface DataMap {
-    meta: {
-      /**
-       * Description of this document.
-       *
-       * Populated by `rehype-infer-description-meta` from the HTML.
-       */
-      description?: string
+  interface DataMapMeta extends InferDescriptionMeta {}
 
-      /**
-       * Description of this document, in hast form.
-       *
-       * Populated by `rehype-infer-description-meta` from the HTML.
-       */
-      descriptionHast?: Root | Element
-    }
+  interface DataMap {
+    meta: DataMapMeta
   }
 }
+
+// To do: remove.
+// eslint-disable-next-line @typescript-eslint/no-useless-empty-export
+export {}
