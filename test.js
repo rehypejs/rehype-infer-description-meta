891cc9bb709743ec5c34b04e418f7cbc7c3abc47
Titus Wormer <tituswormer@gmail.com>
2023-09-02 10:07:20 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -1,151 +1,155 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {rehype} from 'rehype'
 import {u} from 'unist-builder'
 import {h} from 'hastscript'
 import rehypeMeta from 'rehype-meta'
 import rehypeInferDescriptionMeta from './index.js'
 
-test('rehypeInferDescriptionMeta', async (t) => {
-  let file = await rehype()
-    .use({settings: {fragment: true}})
-    .use(rehypeInferDescriptionMeta)
-    .process(
-      '<div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>'
-    )
+test('rehypeInferDescriptionMeta', async function (t) {
+  await t.test('should truncate the document by default', async function () {
+    const file = await rehype()
+      .use({settings: {fragment: true}})
+      .use(rehypeInferDescriptionMeta)
+      .process(
+        '<div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>'
+      )
 
-  t.deepEqual(
-    file.data,
-    {
+    assert.deepEqual(file.data, {
       meta: {
         description:
           'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim…'
       }
-    },
-    'should truncate the document by default'
-  )
+    })
+  })
 
-  file = await rehype()
-    .use({settings: {fragment: true}})
-    .use(rehypeInferDescriptionMeta, {inferDescriptionHast: true})
-    .process(
-      '<div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>'
-    )
+  await t.test(
+    'should support `inferDescriptionHast: true`',
+    async function () {
+      const file = await rehype()
+        .use({settings: {fragment: true}})
+        .use(rehypeInferDescriptionMeta, {inferDescriptionHast: true})
+        .process(
+          '<div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>'
+        )
 
-  t.deepEqual(
-    file.data,
-    {
-      meta: {
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim…',
-        descriptionHast: u('root', {data: {quirksMode: false}}, [
-          h('div', [
-            h(
-              'p',
-              'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-            ),
-            h('p', 'Ut enim ad minim…')
+      assert.deepEqual(file.data, {
+        meta: {
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim…',
+          descriptionHast: u('root', {data: {quirksMode: false}}, [
+            h('div', [
+              h(
+                'p',
+                'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+              ),
+              h('p', 'Ut enim ad minim…')
+            ])
           ])
-        ])
-      }
-    },
-    'should support `inferDescriptionHast: true`'
+        }
+      })
+    }
   )
 
-  file = await rehype()
-    .use({settings: {fragment: true}})
-    .use(rehypeInferDescriptionMeta, {mainSelector: 'article'})
-    .process('<p>Lorem ipsum dolor sit amet.</p>')
+  await t.test(
+    'should support `mainSelector` to select the body (1)',
+    async function () {
+      const file = await rehype()
+        .use({settings: {fragment: true}})
+        .use(rehypeInferDescriptionMeta, {mainSelector: 'article'})
+        .process('<p>Lorem ipsum dolor sit amet.</p>')
 
-  t.deepEqual(
-    file.data,
-    {},
-    'should support `mainSelector` to select the body (1)'
+      assert.deepEqual(file.data, {})
+    }
   )
 
-  file = await rehype()
-    .use({settings: {fragment: true}})
-    .use(rehypeInferDescriptionMeta, {mainSelector: 'article'})
-    .process(
-      '<aside><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></aside><article><p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></article>'
+  await t.test(
+    'should support `mainSelector` to select the body (2)',
+    async function () {
+      const file = await rehype()
+        .use({settings: {fragment: true}})
+        .use(rehypeInferDescriptionMeta, {mainSelector: 'article'})
+        .process(
+          '<aside><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></aside><article><p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></article>'
+        )
+
+      assert.deepEqual(file.data, {
+        meta: {
+          description:
+            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in…'
+        }
+      })
+    }
+  )
+
+  await t.test(
+    'should support `selector` to select the description',
+    async function () {
+      const file = await rehype()
+        .use({settings: {fragment: true}})
+        .use(rehypeInferDescriptionMeta, {selector: '.byline'})
+        .process(
+          '<p class=byline>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+        )
+
+      assert.deepEqual(file.data, {
+        meta: {
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        }
+      })
+    }
+  )
+
+  await t.test(
+    'should support `comment` to select the description',
+    async function () {
+      const file = await rehype()
+        .use({settings: {fragment: true}})
+        .use(rehypeInferDescriptionMeta, {comment: 'summary'})
+        .process(
+          '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <!-- summary --> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+        )
+
+      assert.deepEqual(file.data, {
+        meta: {
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        }
+      })
+    }
+  )
+
+  await t.test(
+    'should support `selector` to select the description',
+    async function () {
+      const file = await rehype()
+        .use({settings: {fragment: true}})
+        .use(rehypeInferDescriptionMeta, {ignoreSelector: '.ignore'})
+        .process(
+          '<p>Lorem ipsum dolor sit amet, <span class=ignore>consectetur adipisicing elit, </span>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>'
+        )
+
+      assert.deepEqual(file.data, {
+        meta: {
+          description:
+            'Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        }
+      })
+    }
+  )
+
+  await t.test('should integrate w/ `rehype-meta`', async function () {
+    const file = await rehype()
+      .use(rehypeInferDescriptionMeta)
+      .use(rehypeMeta)
+      .process(
+        '<h1>Hello, world</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+      )
+
+    assert.equal(
+      String(file),
+      '<html><head>\n<meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad…">\n</head><body><h1>Hello, world</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></body></html>'
     )
-
-  t.deepEqual(
-    file.data,
-    {
-      meta: {
-        description:
-          'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in…'
-      }
-    },
-    'should support `mainSelector` to select the body (2)'
-  )
-
-  file = await rehype()
-    .use({settings: {fragment: true}})
-    .use(rehypeInferDescriptionMeta, {selector: '.byline'})
-    .process(
-      '<p class=byline>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
-    )
-
-  t.deepEqual(
-    file.data,
-    {
-      meta: {
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-      }
-    },
-    'should support `selector` to select the description'
-  )
-
-  file = await rehype()
-    .use({settings: {fragment: true}})
-    .use(rehypeInferDescriptionMeta, {comment: 'summary'})
-    .process(
-      '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <!-- summary --> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
-    )
-
-  t.deepEqual(
-    file.data,
-    {
-      meta: {
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-      }
-    },
-    'should support `comment` to select the description'
-  )
-
-  file = await rehype()
-    .use({settings: {fragment: true}})
-    .use(rehypeInferDescriptionMeta, {ignoreSelector: '.ignore'})
-    .process(
-      '<p>Lorem ipsum dolor sit amet, <span class=ignore>consectetur adipisicing elit, </span>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>'
-    )
-
-  t.deepEqual(
-    file.data,
-    {
-      meta: {
-        description:
-          'Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-      }
-    },
-    'should support `selector` to select the description'
-  )
-
-  file = await rehype()
-    .use(rehypeInferDescriptionMeta)
-    .use(rehypeMeta)
-    .process(
-      '<h1>Hello, world</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
-    )
-
-  t.equal(
-    String(file),
-    '<html><head>\n<meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad…">\n</head><body><h1>Hello, world</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></body></html>',
-    'should integrate w/ `rehype-meta`'
-  )
-
-  t.end()
+  })
 })
